2594ad408fb7fede29fa39a47c076630bc818bbf
profbunbun <aaronrls1984@gmail.com>
2023-12-05 16:09:04 -0500

    works

--- bootloader.asm
+++ bootloader.asm
@@ -1,20 +1,19 @@
-; bootloader.asm
-[org 0x7c00]
-bits 16
-jmp near start
-start:
-    mov ah, 0x0E ; BIOS teletype function
-    mov al, 'H'
-    int 10h
-    mov al, 'e'
-    int 10h
-    mov al, 'l'
-    int 10h
-    mov al, 'l'
-    int 10h
-    mov al, 'o'
-    int 10h
-    jmp $
+org 0x7C00                      
+bits 16                     
 
-times 510 - ($ - $$) db 0 ; Fill the rest of the 512-byte bootloader with zeros
-dw 0xAA55 ; Boot signature
+start:
+	cli                   
+	mov si, msg           
+	mov ah, 0x0E        
+.loop	lodsb               
+	or al, al             
+	jz halt                 
+	int 0x10        
+	jmp .loop               
+
+halt:	hlt                     
+msg:	db "BOOOOOOOOTY!", 0   
+
+;; Magic numbers
+times 510 - ($ - $$) db 0
+dw 0xAA55
